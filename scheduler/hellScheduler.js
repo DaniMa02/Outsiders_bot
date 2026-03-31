61561f4ea0824b973374bf8c5099c337b107dcb5
DaniMa02 <danielmiguela411@gmail.com>
2026-03-31 21:51:18 +0200

    Cambio hora

--- scheduler/hellScheduler.js
+++ scheduler/hellScheduler.js
@@ -17,7 +17,7 @@ const HELL_SCHEDULE = {
 
 export const startHellScheduler = (client) => {
   cron.schedule(
-    '40 21 * * *', // ⬅️ cambia temporalmente esto para testear
+    '55 21 * * *', // ⬅️ cambia temporalmente esto para testear
     async () => {
       try {
         console.log('⏰ Ejecutando hellScheduler...');
